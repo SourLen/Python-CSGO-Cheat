c44976b5056d0ef3b31662f0343e226e2b7f8aa0
XanaX <62156037+XanOpiat@users.noreply.github.com>
2021-10-21 19:38:59 +0200

    Update Triggerbot.py

--- Utils/Triggerbot.py
+++ Utils/Triggerbot.py
@@ -3,6 +3,6 @@ from Utils.Offsets import *
 
 
 
-def shootTrigger(pm, CrossID, client, lTeam, CTeam):
+def shootTrigger(pm, CrossID, client, lTeam, CTeam, triggerkey):
     if keyboard.is_pressed(triggerkey) and 0 < CrossID < 64 and lTeam != CTeam:
         pm.write_int(client + dwForceAttack, 6)

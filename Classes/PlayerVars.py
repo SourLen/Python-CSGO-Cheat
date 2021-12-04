8775f30df0f4156ac5a684f373853b3b6103c311
XanaX <62156037+XanOpiat@users.noreply.github.com>
2021-12-04 15:44:51 +0100

    Update PlayerVars.py

--- Classes/PlayerVars.py
+++ Classes/PlayerVars.py
@@ -3,9 +3,9 @@ from Utils.Offsets import *
 
 
 def GetPlayerVars(pm, client, engine, engine_pointer, player):
-    player = pm.read_int( client + dwLocalPlayer )
-    engine_pointer = pm.read_int( engine + dwClientState )
-    glow_manager = pm.read_int( client + dwGlowObjectManager )
+    player = pm.read_uint( client + dwLocalPlayer )
+    engine_pointer = pm.read_uint( engine + dwClientState )
+    glow_manager = pm.read_uint( client + dwGlowObjectManager )
     crosshairID = pm.read_int( player + m_iCrosshairId )
     getcrosshairTarget = pm.read_int( client + dwEntityList + (crosshairID - 1) * 0x10 )
     immunitygunganme = pm.read_int( getcrosshairTarget + m_bGunGameImmunity )

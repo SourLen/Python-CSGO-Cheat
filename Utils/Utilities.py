b0c7456e87471b67aa2b4ed08df9a37196a32206
XanOpiat <sauer.lennart@gmail.com>
2021-12-30 21:00:07 +0100

    Small Mistake in VC

--- Utils/Utilities.py
+++ Utils/Utilities.py
@@ -24,7 +24,7 @@ def GetForegroundWindow():
 def update():
     gn = get_netvars(pymem.Pymem("csgo.exe"))
     raw = requests.get("https://raw.githubusercontent.com/XanOpiat/Python-CSGO-Cheat/main/Utils/Utilities.py").text
-    vc = raw.splitlines()[1].split("=")[-1][2:-1]
+    vc = raw.splitlines()[3].split("=")[-1][2:-1]
     if vc == versioncontrol:
         print("Cheat up to date")
         return True

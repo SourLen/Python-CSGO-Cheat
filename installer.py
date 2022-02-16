a79d5394a2dbd7204f781ee75ca9a64d4f61c136
= <=>
2022-02-16 13:22:40 -0300

    🎨 Install script code improvements

--- installer.py
+++ installer.py
@@ -1,104 +1,58 @@
+import os
 import sys
 import subprocess
-import os
 
-# Vars
-missingPkgs = 0
+clear = lambda: os.system('cls')
 
+def install(package: str):
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
 
-# Cleaned most of this file,
-# This file is now working.
-# Last update 2022,Jan,10
+def is_installed(package: str):
+    try:
+        __import__(package)
+        return True
+    
+    except ImportError:
+        return False
+    
+    except Exception as e:
+        print(f'Unexpected error: {e} while checking if {package} is installed')
+        return True
+
+def get_pending_packages():
+    pending = []
+    with open('requirements.txt', 'rt') as f:
+        requirements = f.read().splitlines()
+        for package in requirements:
+            if package and not is_installed(package):
+                pending.append(package)
+    return pending
 
 def setup():
-    global missingPkgs
-    # Testing imports
-    try:
-        import pymem
-        print(pymem.__name__)
-    except ImportError:
-        missingPkgs += 1
+    pending = get_pending_packages()
 
-    try:
-        import keyboard
-        print(keyboard.__name__)
-    except ImportError:
-        missingPkgs += 1
+    if not pending:
+        return print('All packages are installed')
 
-    try:
-        import PyQt5
-        print(PyQt5.__name__)
-    except ImportError:
-        missingPkgs += 1
+    print(f'{len(pending)} missing packages.\n\tWould you like to install them? (y/n)')
 
-    try:
-        import threaded
-        print(threaded.__name__)
-    except ImportError:
-        missingPkgs += 1
-
-    try:
-        import requests
-        print(requests.__name__)
-    except ImportError:
-        missingPkgs += 1
-
-    try:
-        import mouse
-        print(mouse.__name__)
-    except ImportError:
-        missingPkgs += 1
-
-    # Ask to install missing packages!
-    if missingPkgs == 0:
-        os.system("cls")
-        print("You already installed the required packages!")
-        quit(0)
+    if input().lower() == 'y':
+        for package in pending:
+            print(f'Installing {package}...')
+            install(package)
+        print('Installation complete')
     else:
-        os.system("cls")
-        print(f"You are missing [{missingPkgs}] packages! \n\tWould you like to install them? \n\t(Y/N)")
-        installSTR = str(input("Install? > "))
-        if installSTR.lower() == "y":
-            os.system("cls")
-            try:
-                import pymem
-            except ImportError:
-                subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pymem'])
-            try:
-                import keyboard
-            except ImportError:
-                subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'keyboard'])
-            try:
-                import PyQt5
-            except ImportError:
-                subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'PyQt5'])
-            try:
-                import threaded
-            except ImportError:
-                subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'threaded'])
-            try:
-                import requests
-            except ImportError:
-                subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'requests'])
-            try:
-                import mouse
-            except ImportError:
-                subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'mouse'])
-            os.system("cls")
-            print("Finished installing packages!")
-            quit(0)
-        elif installSTR.lower() == "n":
-            os.system("cls")
-            print("Process aborted!")
-            quit(0)
-        else:
-            os.system("cls")
-            print("Process aborted! \n\t(Y/N) Y = Yes | N = No")
-            quit(0)
+        print('Installation cancelled')   
 
+def main():
+    try:
+        clear()
+        setup()
+
+    except KeyboardInterrupt:
+        print('Installation cancelled by user')
 
 if __name__ == '__main__':
-    setup()
+    main()
 else:
-    print("Installer Is not allowed to be ran, by other programs!")
-    quit(0)
+    print("Installer is not allowed to be ran, by other programs!")
